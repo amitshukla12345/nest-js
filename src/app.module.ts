963d78d30d5af136877770a32d4792042baeafae
Amit Shukla <amitshukla22509@gmail.com>
2025-07-24 04:48:47 -0700

    third commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,18 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    
 
 
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+    /*  host: 'localhost',*/
       port: 3306,
       username: 'root',
       password: 'Wj24@Amit',
       database: 'dashboard',
       autoLoadEntities: true,
+      synchronize: true,
        
     
     }),
