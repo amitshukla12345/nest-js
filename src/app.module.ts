11239168d7e1689437c9f53b69440126a2f14594
Amit Shukla <amitshukla22509@gmail.com>
2025-07-26 01:49:46 -0700

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
+  port: Number(process.env.DB_PORT) || 3000,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'dashboard',
